4cd8458d9658890f740217bde3969bc56d523e48
Hakan Akçınar <44376537+hakcinar@users.noreply.github.com>
2022-04-23 04:58:27 +0300

    Solution of Task

--- course/src/Components/ExpenseItem.js
+++ course/src/Components/ExpenseItem.js
@@ -1,16 +1,15 @@
 import React from "react";
 import './ExpenseItem.css';
+import './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
    /* const expenseDate = new Date(2021,2,28);
     const expenseTitle = 'CarInsurance';
     const expenseAmount = 294;*/
+  
   return (
     <div className="expense-item">
-      <div>
-          <div>Month</div>
-          <div>Year</div>
-          <div>Day</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
