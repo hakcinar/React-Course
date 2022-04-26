8461ad130ee44d11941b55a6d5326eb83ab84a26
Hakan Akçınar <44376537+hakcinar@users.noreply.github.com>
2022-04-26 04:43:37 +0300

    Adding Components
    Expenses added

---
+++ course/src/Components/NewExpense/ExpenseForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+function ExpenseForm() {
+    const titleChangeHandler = (event) =>{
+        console.log(event.target.value)
+    }
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>title</label>
+          <input type="text" onChange={titleChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+          <label>amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" step="2022-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
